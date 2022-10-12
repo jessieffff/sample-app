70f67c39e0b80cdf32a0fb465e2250f9188f9c4b
jessiefff <jiefangli0605@gmail.com>
2022-10-12 18:58:33 -0400

    finish user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
 
   private
-
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
